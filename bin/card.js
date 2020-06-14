b2c1901dc4a5a077b9c85d892f49188ca9ee43ed
Luiz Nickel <luizaugustonickel@gmail.com>
2020-06-14 12:01:30 -0300

    feat: add my infos and update the package description

--- bin/card.js
+++ bin/card.js
@@ -7,51 +7,33 @@ const argv = require("yargs").argv;
 const lang = argv.lang || "en";
 
 const options = {
-  padding: 1,
-  margin: 2,
+  padding: 2,
   borderStyle: "double",
-  borderColor: "#F20089",
+  borderColor: "#50b697",
 };
 
 const data = {
-  logo: chalk.white(`     __ _____ 
-  __|  |     |
- |  |  |  |  |
- |_____|_____|
-  _____ _____ 
- |   __|   __|
- |__   |   __|
- |_____|_____|`),
-  name: chalk.white(" Joselito /"),
-  mail: {
-    en: chalk.hex("#F20089")("hey@joseli.to"),
-    pt: chalk.hex("#F20089")("oi@joseli.to"),
-  },
+  logo: chalk.white(`<vaporwavie>`),
+  name: chalk.white("Luiz Nickel"),
   pronoun: {
     en: chalk.white("(he/him/his)"),
     pt: chalk.white("(ele/dele)"),
   },
   work: {
-    en: chalk.white("Tech Lead @ StoneCo"),
-    pt: chalk.white("Tech Lead / Stone Pagamentos"),
+    en: chalk.white("   Front-end Lead @ Sizebay"),
+    pt: chalk.white("   Front-end Lead / Sizebay"),
   },
-  twitter: chalk.hex("#F20089")("https://twitter.com/breakzplatform"),
-  github: chalk.hex("#F20089")("https://github.com/breakzplatform"),
-  unsplash: chalk.hex("#F20089")("https://unsplash.com/@breakzplatform"),
-  picpay: chalk.hex("#F20089")("https://picpay.me/joselitojunior"),
-  web: chalk.hex("#F20089")("https://joseli.to"),
-  npx: chalk.white("npx joselito"),
+  resume: chalk.hex("#50b697")(" https://github.com/vaporwavie/resume"),
+  twitter: chalk.hex("#50b697")("https://twitter.com/vaporwavie"),
+  github: chalk.hex("#50b697")(" https://github.com/vaporwavie"),
 };
 
 const defaultLabel = {
   en: {
-    work: chalk.white.bold("      Work:"),
-    twitter: chalk.white.bold("   Twitter:"),
-    github: chalk.white.bold("    GitHub:"),
-    picpay: chalk.white.bold("    PicPay:"),
-    unsplash: chalk.white.bold("  Unsplash:"),
-    web: chalk.white.bold("       Web:"),
-    npx: chalk.white.bold("      Card:"),
+    work: chalk.white.bold("Work:"),
+    resume: chalk.white.bold("Resume:"),
+    twitter: chalk.white.bold("Twitter:"),
+    github: chalk.white.bold("GitHub:"),
   },
 };
 
@@ -59,28 +41,22 @@ const label = {
   ...defaultLabel,
   pt: {
     ...defaultLabel.en,
-    work: chalk.white.bold("  Trabalho:"),
-    npx: chalk.white.bold("    Cartão:"),
+    work: chalk.white.bold("Trabalho:"),
   },
 };
 
-const output = `${data.logo}
-
-${data.name} ${data.mail[lang]} ${data.pronoun[lang]}
-
+const output = `
+${data.name} ${data.logo} ${data.pronoun[lang]}
 
 ${label[lang].work}  ${data.work[lang]}
 
-${label[lang].web}  ${data.web}
-
 ${label[lang].twitter}  ${data.twitter}
 ${label[lang].github}  ${data.github}
-${label[lang].unsplash}  ${data.unsplash}
-${label[lang].picpay}  ${data.picpay}
-
-${label[lang].npx}  ${data.npx}`;
+${label[lang].resume}  ${data.resume}
+`;
 
 console.clear();
 console.log(`
 `);
-console.log(chalk.magenta(boxen(output, options)));
+
+console.log(boxen(output, options));
